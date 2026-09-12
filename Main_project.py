b882e7ff4a21740d351a44036ceb1826cd9f5811
shubhambyte-by <shubhamsahu1771@gmail.com>
2026-09-12 10:41:39 +0530

    Making output layout

---
+++ Main_project.py
@@ -0,0 +1,23 @@
+# last output format
+
+for i in range(0,31):
+    print('--', end = "")
+
+print(" \n                      TI Supermarket London UK ")
+
+for i in range(0,31):
+    print('--', end = "")
+
+print("\n ||        Name          || ","Shubham Sahu")
+for i in range(0,31):
+    print('--', end = "")
+
+print("\n ||     Product Name     || ","Mango ")
+for i in range(0,31):
+    print('--', end = "")
+print("\n ||         Qty          || ","2")
+for i in range(0,31):
+    print('--', end = "")
+print("\n ||  Product price + GST || ","")
+for i in range(0,31):
+    print('--', end = "")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+
+print("\n ||        Name          || ","Shubham Sahu")
+for i in range(0,31):
+    print('--', end = "")
